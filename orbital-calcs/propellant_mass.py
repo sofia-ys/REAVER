d1d9186b05482454a56ba0b4879191e5149512d4
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2026-05-08 16:13:27 +0200

    okay now it's fixed <3

--- orbital-calcs/propellant_mass.py
+++ orbital-calcs/propellant_mass.py
@@ -37,17 +37,34 @@ def rcs_m(m_prop):
 # dv_list is in [km/s]
 # m_dry is the mass of the spacecraft without ANY propellant [kg]
 # n_targets is how many debris are being captured
-def prop_mass_multiTarget_iteration(m_dry, n_targets, dv_list):
+def prop_mass_multiTarget(m_dry, n_targets, dv_list):
     
     m_prop = 0
+    m_prop_list = []
     for i in range(n_targets + 1):
-        m_final = m_dry + (n_targets - i) * m_debris + m_prop + rcs_m(m_prop)
+        m_final = m_dry + (n_targets - i) * m_debris + m_prop 
         m_prop += propellant_m(dv_list[-(i+1)], Isp, m_final)
+        m_prop_list.append(m_prop)
 
-    return m_prop
+    return m_prop, m_prop_list[::-1]
 
 # here adjust your parameters
 n_targets = 2
 dv_list = [0.6880360287114226, 0.5325970850852726, 0.15780588989264369]
-m_prop = prop_mass_multiTarget_iteration(m_dry, n_targets, dv_list)
-print(m_prop)
+
+# iterating to include propellant tanks dry mass as a part of the dry mass
+m_prop = 0  # initialising our propellant mass estimates
+m_prop_prev = 1 
+while abs(m_prop - m_prop_prev) > 1e-6:  # convergence condition
+    m_prop_prev = m_prop  # setting the previous estimate so we can compare
+    m_prop, m_prop_list = prop_mass_multiTarget(m_dry + rcs_m(m_prop), n_targets, dv_list)
+
+# just for fancy printing
+manouevre_list = ""
+for j in range(1, n_targets + 1):
+    manouevre_list += "DEBRIS-" + str(j) + " → "
+
+print(f"========================{n_targets}-DEBRIS TRAJECTORY========================")
+print(f"Total propellant mass: {m_prop} [kg]")
+print(f"Manouevre trajectory: RH → {manouevre_list}RH ")
+print(f"Propellant mass at the start of each manouevre: {m_prop_list} [kg]")
