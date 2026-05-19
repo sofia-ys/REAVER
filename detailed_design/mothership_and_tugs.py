b0bf62a73390881887bf471f98a5de578b42ec5a
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2026-05-19 14:11:13 +0200

    no hardcoded contingency now, just unmargined values

--- detailed_design/mothership_and_tugs.py
+++ detailed_design/mothership_and_tugs.py
@@ -5,9 +5,12 @@ tug_wet_mass = tug.m_prop + tug.m_dry
 mothership = SpaceCraft(sc_type="ms", m_wet_t=tug_wet_mass)
 
 print("===============================DRY MASS===============================")
-print(f"TUG: {tug.m_dry * 1.2}")
-print(f"MOTHERSHIP: {mothership.m_dry * 1.2}")
+print(f"TUG: {tug.m_dry}")
+print(f"MOTHERSHIP: {mothership.m_dry}")
 
 print("============================PROPELLANT MASS============================")
 print(f"TUG: {tug.m_prop}")
 print(f"MOTHERSHIP: {mothership.m_prop}")
+
+print("==============================TOTAL MASS==============================")
+print(f"LAUNCH MASS: {4*(tug.m_dry + tug.m_prop) + (mothership.m_dry + mothership.m_prop)}")
