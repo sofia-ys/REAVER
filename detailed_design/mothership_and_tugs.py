b55bc537a0f529500076ea8442be0071b7a8a79c
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2026-05-20 13:58:08 +0200

    improving readbilty

--- detailed_design/mothership_and_tugs.py
+++ detailed_design/mothership_and_tugs.py
@@ -1,26 +1,28 @@
 from mission_spacecraft import SpaceCraft
 
+n_tugs = 4  # number of tugs 
 tug = SpaceCraft(sc_type="tug")
 tug_wet_mass = tug.m_prop + tug.m_dry
 mothership = SpaceCraft(sc_type="ms", m_wet_t=tug_wet_mass)
 
 print("===============================DRY MASS===============================")
 print(f"EACH TUG: {tug.m_dry}")
-print(f"ALL TUGS (5x): {5 * tug.m_dry}")
+print(f"ALL TUGS ({n_tugs}x): {n_tugs * tug.m_dry}")
 print(f"MOTHERSHIP: {mothership.m_dry}")
 
-print("============================PROPELLANT MASS============================")
+print("============================PROPELLANT MASS===========================")
 print(f"EACH TUG: {tug.m_prop}")
-print(f"ALL TUGS (5x): {5 * tug.m_prop}")
+print(f"ALL TUGS ({n_tugs}x): {n_tugs * tug.m_prop}")
 print(f"MOTHERSHIP: {mothership.m_prop}")
 
 print("==============================TOTAL MASS==============================")
-print(f"LAUNCH MASS: {5*(tug.m_dry + tug.m_prop) + (mothership.m_dry + mothership.m_prop)}")
+print(f"TOTAL DRY MASS: {n_tugs * tug.m_dry + mothership.m_dry}")
+print(f"LAUNCH MASS (DRY + PROPELLANT): {n_tugs * (tug.m_dry + tug.m_prop) + (mothership.m_dry + mothership.m_prop)}")
 
-print("===========================TUG MASS BREAKDOWN===========================")
+print("==========================TUG MASS BREAKDOWN==========================")
 for item in tug.mass_breakdown():
     print(f"{item}: {tug.mass_breakdown()[item]} [kg]")
 
-print("========================MOTHERSHIP MASS BREAKDOWN========================")
+print("=======================MOTHERSHIP MASS BREAKDOWN======================")
 for item in mothership.mass_breakdown():
     print(f"{item}: {mothership.mass_breakdown()[item]} [kg]")
