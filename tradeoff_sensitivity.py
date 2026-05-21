e8519caf812f80a646fcc32914ada6c133fa4930
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2026-05-21 12:53:44 +0200

    all criteria at bottom for easy copy paste

--- tradeoff_sensitivity.py
+++ tradeoff_sensitivity.py
@@ -67,3 +67,124 @@ def bar_chart_analysis(criteria, mission_scores):
     return winner_count
 
 bar_chart_analysis(criteria, mission_scores)
+
+
+'''CAPTURE MECHANISM'''
+# criteria_input = {
+#     "mass": 0.2,
+#     "tumbling_performance": 0.1,
+#     "docking_performance": 0.25,
+#     "operational_flexibility": 0.1,
+#     "reliability_and_redundancy": 0.25,
+#     "TRL": 0.1
+# }
+# mission_scores_input = {
+#     "RM":       [3, 4, 5, 5, 5, 4],
+#     "GAG":      [5, 3, 4, 4, 3, 4],
+#     "Clamp":    [4, 3, 4, 4, 5, 4],
+#     "PA":       [4, 3, 4, 3, 5, 5],
+#     "MEV-M":    [5, 3, 4, 3, 5, 5]
+# }
+
+'''CHEMICAL PROPULSION'''
+# criteria_input = {
+#     "isp": 0.2,
+#     "thurst": 0.1,
+#     "storage": 0.25,
+#     "TRL": 0.1
+# }
+# mission_scores_input = {
+#     "LMP-103S":     [3, 5, 5, 5],
+#     "ASCENT/ADN":   [2, 3, 4, 5],
+#     "SHP163/HAN":   [4, 2, 4, 5],
+#     "HTP-kerosene": [4, 5, 4, 3]
+# }
+
+'''ELECTRIC PROPULSION'''
+# criteria_input = {
+#     "isp": 0.15,
+#     "thurst": 0.3,
+#     "dry_mass": 0.2,
+#     "power": 0.2,
+#     "TRL": 0.15
+# }
+# mission_scores_input = {
+#     "NEXT-C":       [3, 5, 2, 2, 5],
+#     "PPS-1350E":    [3, 4, 4, 2, 5],
+#     "PPS-1350S":    [3, 3, 4, 3, 5],
+#     "PPS-5000":     [3, 5, 3, 2, 5],
+#     "PPS-X00":      [3, 3, 5, 3, 4],
+#     "Solar Sail":   [5, 1, 3, 5, 3]
+# }
+
+'''SUSTAINABILITY'''
+# criteria_input = {
+#     "lca": 0.35,
+#     "collision_probability": 0.25,
+#     "propulsion_efficiency": 0.4
+# }
+# mission_scores_input = {
+#     "STR":  [3, 4, 2],
+#     "MTR":  [4, 2, 2],
+#     "PSW":  [2, 4, 2],
+#     "M&T":  [2, 3, 4],
+#     "ISC":  [2, 2, 5]
+# }
+
+'''FEASIBILITY & RELIABILITY'''
+# criteria_input = {
+#     "cost": 0.25,
+#     "complexity": 0.15,
+#     "schedule": 0.2,
+#     "redundancy": 0.15,
+#     "risk": 0.25
+# }
+# mission_scores_input = {
+#     "STR":  [4, 5, 4, 1, 2],
+#     "MTR":  [3, 2, 3, 1, 1],
+#     "PSW":  [1, 1, 3, 5, 5],
+#     "M&T":  [2, 2, 3, 4, 3],
+#     "ISC":  [2, 4, 3, 5, 4]
+# }
+
+'''INNOVATION'''
+# criteria_input = {
+#     "team_preference": 0.4,
+#     "technical_originality": 0.25,
+#     "future_potential": 0.35
+# }
+# mission_scores_input = {
+#     "STR":  [1, 2, 3],
+#     "MTR":  [1, 3, 4],
+#     "PSW":  [1, 4, 4],
+#     "M&T":  [5, 4, 4],
+#     "ISC":  [1, 2, 3]
+# }
+
+'''TECHNICAL PERFORMANCE'''
+# criteria_input = {
+#     "time": 0.4,
+#     "equivalent_mass": 0.6
+# }
+# mission_scores_input = {
+#     "STR":  [3, 3],
+#     "MTR":  [5, 2],
+#     "PSW":  [3, 4],
+#     "M&T":  [3, 5],
+#     "ISC":  [4, 1]
+# }
+
+'''MISSION CONCEPT'''
+# criteria_input = {
+#     "innovation": 0.15,
+#     "technical_performance": 0.35,
+#     "sustainability": 0.2,
+#     "feasibility_and_reliability": 0.3
+# }
+# mission_scores_input = {
+#     "STR":  [1.95, 3.6, 2.85, 3.2],
+#     "MTR":  [2.55, 3.2, 2.7, 2.05],
+#     "PSW":  [2.8, 3.6, 2.5, 3],
+#     "M&T":  [4.4, 4.2, 3.05, 2.75],
+#     "ISC":  [1.95, 2.2, 3.2, 3.45]
+# }
