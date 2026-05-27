8880839366f7f549245d73f05d54cc11d4a91ebb
rdussaud <rdussaud@tudelft.nl>
2026-05-27 09:16:45 +0200

    for jb

--- debrisdata/target_selection.py
+++ debrisdata/target_selection.py
@@ -9,10 +9,8 @@ INC_MIN_DEG     = 0.0
 INC_MAX_DEG     = 20.0
 MASS_MIN_KG     = 1_000/0.7
 MASS_MAX_KG     = 2_000/0.7
-MAX_ECC = 0.001
 
-data = data[(data['MASS_KG'] >= 1500) & (data['MASS_KG'] <= 2900)]
-data = data[(data['ECCENTRICITY']<=0.001)]
+print(f"Before country filter: {len(data)}")
 
 ######################
 ### country filter ###
@@ -20,7 +18,7 @@ data = data[(data['ECCENTRICITY']<=0.001)]
 
 # Allowed ownership/operator codes
 ALLOWED_COUNTRIES = {
-    "US","JPN","GER","NETH","UK","SWED","NOR","EUTE","SES","ITSO",
+    "GER","NETH","UK","SWED","NOR","EUTE","EUME"
 }
 # Name of the dataframe column containing country/operator codes
 COUNTRY_CODE = "COUNTRY_CODE"
@@ -29,12 +27,7 @@ COUNTRY_CODE = "COUNTRY_CODE"
 if COUNTRY_CODE in data.columns:
 
     # Clean formatting
-    data[COUNTRY_CODE] = (
-        data[COUNTRY_CODE]
-        .astype(str)
-        .str.upper()
-        .str.strip()
-    )
+    data[COUNTRY_CODE] = (data[COUNTRY_CODE].astype(str).str.upper().str.strip())
 
     # Apply filter
     data = data[(data[COUNTRY_CODE].isin(ALLOWED_COUNTRIES))]
@@ -44,4 +37,12 @@ if COUNTRY_CODE in data.columns:
 else:
     print(f"{COUNTRY_CODE} column not found")
 
-print(data.columns.tolist())
+
+#Engine compliance with capture mechanism
+
+
+######################
+#### Optimization ####
+######################
+
+
