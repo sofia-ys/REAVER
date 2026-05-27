4b53690ac17823580fe9eeffa78e371132cfc7c3
rdussaud <rdussaud@tudelft.nl>
2026-05-27 10:55:17 +0200

    Pre-opti

--- debrisdata/target_selection.py
+++ debrisdata/target_selection.py
@@ -29,12 +29,17 @@ if COUNTRY_CODE in data.columns:
 else:
     print(f"{COUNTRY_CODE} column not found")
 
-
-print(data["OBJECT_ID"])
-print(data.loc[628])
+ALLOWED_ENGINES = {
+    "GER","NETH","UK","SWED","NOR","EUTE","EUME"
+}
 
 #Engine compliance with capture mechanism
+remove_rows = [504,520,525,758,960,1002,1216] #Engines are not compatible
+data = data.drop(remove_rows)
 
+#Generate table of desired parameters
+table = data[["OBJECT_NAME","MASS_KG","PERIOD","SEMIMAJOR_AXIS","INCLINATION","RA_OF_ASC_NODE"]]
+print(table)
 
 ######################
 #### Optimization ####
