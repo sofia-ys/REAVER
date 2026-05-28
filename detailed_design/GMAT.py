5ffecb3c12dcfc151e78c751046f530f6553187c
Lauke <labsil@tudelft.nl>
2026-05-28 09:57:37 +0200

    Dear Raphael, 'twas a fierce battle, but a battle well-fought nonetheless. Though as the sun sets, so must our journey end. I wish you godspeed brother, and bid thee farewell

---
+++ detailed_design/GMAT.py
@@ -0,0 +1,26 @@
+from load_gmat import gmat
+
+gmat.Help()
+
+import gmatpyplus as gp
+
+sat_params = {
+    'Name': 'DefaultSC',
+    'Orbit': {
+        'CoordSys': 'EarthMJ2000Eq',
+        'Epoch': '21545',
+        'DisplayStateType': 'Keplerian',
+        'SMA': 7200
+    },
+    'DryMass': 100,  # kg
+    'Hardware': {'ChemicalTanks': [{'Name': 'ChemicalTank1'}],
+                 'ElectricTanks': [{'Name': 'ElectricTank1'}],
+                 'ChemicalThrusters': [{'Name': 'ChemicalThruster1', 'Tanks': 'ChemicalTank1'}],
+                 'ElectricThrusters': [{'Name': 'ElectricThruster1', 'Tanks': 'ElectricTank1'}],
+                 'SolarPowerSystem': {'Name': 'SolarPowerSystem1'},
+                 }
+}
+
+sat = gp.Spacecraft.from_dict(sat_params)
+
+sat.Help()
