e711f2b6de40d85edfe3f4ad027fbd08ab483365
Lauke <labsil@tudelft.nl>
2026-06-02 14:30:46 +0200

    trajectory optimizer is now linked to the debris filter

--- debrisdata/target_filter.py
+++ debrisdata/target_filter.py
@@ -38,5 +38,16 @@ remove_rows = [504,520,525,758,960,1002,1216] #Engines are not compatible
 data = data.drop(remove_rows)
 
 #Generate table of desired parameters
-table = data[["OBJECT_NAME","MASS_KG","PERIOD","SEMIMAJOR_AXIS","INCLINATION","RA_OF_ASC_NODE"]]   #,"PERIOD","SEMIMAJOR_AXIS","INCLINATION","RA_OF_ASC_NODE"
-print(table.to_string())
+table = [
+    (
+        int(idx),
+        row["OBJECT_NAME"],
+        float(row["MASS_KG"]),
+        float(row["SEMIMAJOR_AXIS"]),
+        float(row["INCLINATION"]),
+        float(row["RA_OF_ASC_NODE"])
+    )
+    for idx, row in data.iterrows()
+]
+
+print(table)
